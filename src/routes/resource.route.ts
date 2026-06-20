66f967d8cb2054dcf6717d6c8f00f5ab6c5b9332
kyawthethtun <kyawthe235@gmail.com>
2026-06-20 21:21:38 +0630

    update route name

--- src/routes/resource.route.ts
+++ src/routes/resource.route.ts
@@ -9,10 +9,10 @@ import {
 
 const router = Router();
 
-router.post("/resources", createResource);
-router.get("/resources", getAllResources);
-router.get("/resources/:id", getResourceById);
-router.put("/resources/:id", updateResourceById);
-router.delete("/resources/:id", deleteResourceById);
+router.post("/resource", createResource);
+router.get("/resource", getAllResources);
+router.get("/resource/:id", getResourceById);
+router.put("/resource/:id", updateResourceById);
+router.delete("/resource/:id", deleteResourceById);
 
 export default router;
